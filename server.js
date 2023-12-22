58e13388d52d969301376d9b2c475e3ed0150005
kriti <kriti.jindal.37@gmail.com>
2023-12-22 17:47:23 +0530

    CORS

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ import productRouter from './src/features/product/product.routes.js';
 import UserRouter from './src/features/user/user.routes.js';
 import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
-
+import cors from 'cors';
 
 const server = express();
+var corsOptions = {
+    origin: 'http://localhost:5500'
+}
+
+
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5500')
+//     res.header('Access-Control-Allow-Headers', '*');
+//     res.header('Access-Control-Allow-Methods', '*');
+//     if (req.method == "OPTIONS") {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
 
 server.use(express.json());
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
-server.use("/api/products", jwtAuth, productRouter);
+server.use("/api/products", cors(corsOptions), jwtAuth, productRouter);
 server.use('/api/users', UserRouter);
 
 // 3. Default request handler
